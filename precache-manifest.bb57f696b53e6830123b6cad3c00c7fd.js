b69ad2b85866d38dae5b7d9b5ba8ce0789bbb2d6
PrithiveeK <prithivee@codingmart.com>
2020-05-28 12:22:15 +0530

    Updates

---
+++ precache-manifest.bb57f696b53e6830123b6cad3c00c7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd499537205642c1a5f176351a6aa6f4",
+    "url": "/YoutubeCopy/index.html"
+  },
+  {
+    "revision": "138f6ca4ab97bc504403",
+    "url": "/YoutubeCopy/static/css/main.c2536983.chunk.css"
+  },
+  {
+    "revision": "909a9d962bb536ff572b",
+    "url": "/YoutubeCopy/static/js/2.498bdcfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YoutubeCopy/static/js/2.498bdcfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138f6ca4ab97bc504403",
+    "url": "/YoutubeCopy/static/js/main.919c37fc.chunk.js"
+  },
+  {
+    "revision": "6ab986410054ed7ebdcf",
+    "url": "/YoutubeCopy/static/js/runtime-main.d2097d8b.js"
+  }
+]);
